bd2c5f4e3d1ff185da2fa99f6ba5387362313c5d
David Brown <davidbrown@Davids-iMac.local>
2020-09-22 14:14:58 -0600

    merges

--- __tests__/auth-router.test.js
+++ __tests__/auth-router.test.js
@@ -9,50 +9,50 @@ describe("auth-router", () => {
         .rollback()
         .then(() => db.migrate.latest().then(() => db.seed.run()));
     });
-<<<<<<< HEAD
-    it('registers a user', () => {
-      return supertest(server)
-          .post('/api/auth/register')
-          .send({ email: '@gmail.com', role: 'client', username: 'hello', password: 'helloagain' })
-          .then(res => {
-              expect(res.status).toBe(201)
-          })
-  })
 
-  it('should throw error 400 because no password', () => {
+    it("registers a user", () => {
       return supertest(server)
-          .post('/api/auth/register')
-          .send({ username: 'hello', password: '' })
-          .then(res => {
-              expect(res.status).toBe(400)
-          })
-  })
+        .post("/api/auth/register")
+        .send({
+          email: "@gmail.com",
+          role: "client",
+          username: "hello",
+          password: "helloagain",
+        })
+        .then((res) => {
+          expect(res.status).toBe(201);
+        });
+    });
 
+    it("should throw error 400 because no password", () => {
+      return supertest(server)
+        .post("/api/auth/register")
+        .send({ username: "hello", password: "" })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
   });
 });
 
+describe("POST Login", () => {
+  it("should see if message property shows up", () => {
+    const username = "newaccount";
+    const password = "newpassword";
 
-
-
-  describe('POST Login', () => {
-    it('should see if message property shows up', () => {
-        const username = 'newaccount'
-        const password = 'newpassword'
-
-        return supertest(server)
-            .post('/api/auth/login')
-            .send({ username: username, password: password })
-            .then((res) => {
-                expect(res.body).toHaveProperty('message');
-            })
-    }) 
-    it('should throw a 401 since theres no user data', () => {
-        return supertest(server)
-            .post('/api/auth/login')
-            .send({ username: 'newaccount', password: 'newpassword' })
-            .then((res) => {
-                expect(res.status).toBe(401);
-            })
-    }) 
-})
-
+    return supertest(server)
+      .post("/api/auth/login")
+      .send({ username: username, password: password })
+      .then((res) => {
+        expect(res.body).toHaveProperty("message");
+      });
+  });
+  it("should throw a 401 since theres no user data", () => {
+    return supertest(server)
+      .post("/api/auth/login")
+      .send({ username: "newaccount", password: "newpassword" })
+      .then((res) => {
+        expect(res.status).toBe(401);
+      });
+  });
+});
