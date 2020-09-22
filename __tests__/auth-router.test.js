a994fbb032ae8bd1ff89c8c530393e5be8cd72a0
Faisal Aziz <faziz6006@gmail.com>
2020-09-22 15:05:07 -0500

    testing

--- __tests__/auth-router.test.js
+++ __tests__/auth-router.test.js
@@ -7,7 +7,50 @@ describe("auth-router", () => {
     beforeEach(async () => {
       await db.migrate
         .rollback()
-        .then(() => db.migrate.latest().then(() => db.seed.run));
+        .then(() => db.migrate.latest().then(() => db.seed.run()));
     });
+    it('registers a user', () => {
+      return supertest(server)
+          .post('/api/auth/register')
+          .send({ email: '@gmail.com', role: 'client', username: 'hello', password: 'helloagain' })
+          .then(res => {
+              expect(res.status).toBe(201)
+          })
+  })
+
+  it('should throw error 500 because no password', () => {
+      return supertest(server)
+          .post('/api/auth/register')
+          .send({ username: 'hello', password: '' })
+          .then(res => {
+              expect(res.status).toBe(400)
+          })
+  })
   });
 });
+
+
+
+
+  describe('POST Login', () => {
+    it('should see if message property shows up', () => {
+        const username = 'newaccount'
+        const password = 'newpassword'
+
+        return supertest(server)
+            .post('/api/auth/login')
+            .send({ username: username, password: password })
+            .then((res) => {
+                expect(res.body).toHaveProperty('message');
+            })
+    }) 
+    it('should throw a 401 since theres no user data', () => {
+        return supertest(server)
+            .post('/api/auth/login')
+            .send({ username: 'newaccount', password: 'newpassword' })
+            .then((res) => {
+                expect(res.status).toBe(401);
+            })
+    }) 
+})
+
